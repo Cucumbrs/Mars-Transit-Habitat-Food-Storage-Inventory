03d002ca4234f0c3a761bcc4164fe99fcd370917
Cucumbrs <madeline@nackfamily.com>
2026-04-28 15:23:37 -0500

    Add files via upload

---
+++ TempSensor.h
@@ -0,0 +1,11 @@
+#ifndef TEMPSENSOR_H
+#define TEMPSENSOR_H
+
+#include <Arduino.h>
+
+// Initialize temperature/humidity sensor (call once in setup)
+
+// Poll or process temperature/humidity (call frequently in loop)
+void loopTempSensor();
+
+#endif // TEMPSENSOR_H
